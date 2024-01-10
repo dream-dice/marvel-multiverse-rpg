14b125008c985c932d37a88c26f8bc0528ce17ff
Luke Preston <luke.preston@theaccessgroup.com>
2024-01-10 18:58:45 +0000

    will it read files

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-import discord
-from dotenv import load_dotenv
 import os
+
+import discord
+
+from dotenv import load_dotenv
+
+import robot
 
 load_dotenv()
 
@@ -9,17 +13,21 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    username = message.author
+    if message.content.starts_with('!cap d616'):
+        robot.d616(username)
+        await message.channel.send(robot.display(username))
 
 token = os.environ['TOKEN']
 client.run(token)
--- robot.py
+++ robot.py
@@ -1,75 +1,81 @@
 import json
 from random import randint
 
+
 def save(users):
-  with open("users.json", "w") as outfile:
-    json.dump(users, outfile)
-  
+    with open("users.json", "w") as outfile:
+        json.dump(users, outfile)
+
+
 def load():
-  with open("users.json", "r") as openfile:
-    return json.load(openfile)
+    with open("users.json", "r") as openfile:
+        return json.load(openfile)
+
 
 def d616(username):
-  users = load()
-  users[username] = {
-      "d1": randint(1, 6),
-      "d2": randint(1, 6),
-      "dm": randint(1, 6)
-  }
-  save(users)
-  return users
+    users = load()
+    users[username] = {
+        "d1": randint(1, 6),
+        "d2": randint(1, 6),
+        "dm": randint(1, 6)
+    }
+    save(users)
+    return users
+
 
 def reroll(username, die, edge):
-  users = load()
-  user = users[username] or d616(username)[username]
-  value = randint(1, 6)
-  current = user[die]
-  if edge and die == 'dm' and current == 1:
-    user[die] = current
-  elif edge and die == 'dm' and (value > current or value == 1):
-    user[die] = value
-  elif edge and die == 'd1' and (value > current):
-    user[die] = value
-  elif edge and die == 'd2' and (value > current):
-    user[die] = value
-  save(users)
-  return users
+    users = load()
+    user = users[username] or d616(username)[username]
+    value = randint(1, 6)
+    current = user[die]
+    if edge and die == 'dm' and current == 1:
+        user[die] = current
+    elif edge and die == 'dm' and (value > current or value == 1):
+        user[die] = value
+    elif edge and die == 'd1' and (value > current):
+        user[die] = value
+    elif edge and die == 'd2' and (value > current):
+        user[die] = value
+    save(users)
+    return users
+
 
 def set(username, value):
-  users = load()
-  users[username] = value
-  users[username]
-  save(users)
-  return users
+    users = load()
+    users[username] = value
+    users[username]
+    save(users)
+    return users
 
-def display(username, karma = 0):
-  users = load()
-  user = users[username] or d616(username)
-  sum = user["d1"] + user["d2"] + karma
-  if user["dm"] == 1:
-    sum += 6
-  else:
-    sum += user["dm"]
-  
-  fantastic = ''
-  fantastic_roll = user["dm"]
-  if user["dm"] == 1:
-    fantastic_roll = 'M'
-    fantastic = 'Fantastic!'
-  
-  karma_value = ''
-  if karma > 0:
-    karma_value = ', K ({})'.format(karma)
-  
-  if sum > 19:
-    sum = 19
 
-  return "{}: D1 ({}), D2 ({}), DM ({}){} = {} {}".format(
-      username,
-      user["d1"],
-      user["d2"],
-      fantastic_roll,
-      karma_value,
-      sum,
-      fantastic
+def display(username, karma=0):
+    users = load()
+    user = users[username] or d616(username)
+    sum = user["d1"] + user["d2"] + karma
+    if user["dm"] == 1:
+        sum += 6
+    else:
+        sum += user["dm"]
+
+    fantastic = ''
+    fantastic_roll = user["dm"]
+    if user["dm"] == 1:
+        fantastic_roll = 'M'
+        fantastic = 'Fantastic!'
+
+    karma_value = ''
+    if karma > 0:
+        karma_value = ', K ({})'.format(karma)
+
+    if sum > 19:
+        sum = 19
+
+    return "{}: D1 ({}), D2 ({}), DM ({}){} = {} {}".format(
+        username,
+        user["d1"],
+        user["d2"],
+        fantastic_roll,
+        karma_value,
+        sum,
+        fantastic
     )
