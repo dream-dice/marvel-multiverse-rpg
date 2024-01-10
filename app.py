76bdb16f1590706eeb570a6193092b4df427a2f9
Luke Preston <luke.preston@theaccessgroup.com>
2024-01-10 21:37:46 +0000

    slash for guild only I think

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import os
 
 import discord
-from discord.ext import commands
 
 from dotenv import load_dotenv
 
-# from run_command import run_command
+from run_command import run_command
 
 load_dotenv()
 
@@ -15,32 +14,19 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 
 
-# @client.event
-# async def on_ready():
-#     print(f'We have logged in as {client.user}')
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
 
 
-# @client.event
-# async def on_message(message):
-#     if message.author == client.user:
-#         return
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
-#     response = run_command(message.author.name, message.content)
-#     if response:
-#         await message.channel.send(response)
-
-
-
-intents = discord.Intents.default()
-intents.message_content = True
-
-bot = commands.Bot(command_prefix='$', intents=intents)
-
-@commands.command()
-async def test(ctx):
-    pass
-
-bot.add_command(test)
+    response = run_command(message.author.name, message.content)
+    if response:
+        await message.channel.send(response)
 
 token = os.environ['TOKEN']
 client.run(token)
